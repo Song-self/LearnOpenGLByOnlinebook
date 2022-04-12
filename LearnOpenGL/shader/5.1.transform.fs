d9d2f003335e34f4e683225293076d6745fadd6f
Song <773910552@qq.com>
2022-04-12 12:28:17 +0800

    Transformations

--- LearnOpenGL/shader/5.1.transform.fs
+++ LearnOpenGL/shader/5.1.transform.fs
@@ -3,7 +3,6 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
 in vec2 TexCoord;
 
 // texture samplers
@@ -13,6 +12,6 @@ uniform sampler2D texture2;
 void main()
 {
 	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2); 
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
 
