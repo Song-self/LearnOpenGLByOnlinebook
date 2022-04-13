7504cc0b8ba24cc8670a5dfcacc1f1558a05ca06
Song <773910552@qq.com>
2022-04-13 13:14:03 +0800

    Coordinate Systems

--- LearnOpenGL/src/learnopengl/shader_s.h
+++ LearnOpenGL/src/learnopengl/shader_s.h
@@ -9,6 +9,8 @@
 #include <sstream>
 #include <iostream>
 
+#include <glm/glm.hpp>
+
 class Shader
 {
 public:
@@ -90,6 +92,11 @@ public:
     void setFloat(const std::string& name, float value) const
     {
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+    void setMat4(const std::string& name, glm::mat4 value)
+    {
+        glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()),1, GL_FALSE, glm::value_ptr(value));
     }
 
 private:
